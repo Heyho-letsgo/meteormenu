a2933ba9ded53288947061ee3334d896daabfdda
Baudin-Laurencin <abaudin@gmail.com>
2016-03-15 14:19:02 +0000

    recipe.js ok

--- collections/Recipes.js
+++ collections/Recipes.js
@@ -1,2 +1,29 @@
-Recipes = new Meteor.Collection('recipes');
+Recipes = new Mongo.Collection('recipes');
 
+RecipeSchema = new SimpleSchema({
+    name: {
+        type: String,
+        label: "name"
+    },
+    desc: {
+        type: String,
+        label: "Description"
+    },
+    author: {
+        type: String,
+        label: "Auhor",
+        autoValue: function() {
+            return this.userId
+        }
+    },
+    createdAt: {
+        type: Date,
+        label: "Created At",
+        autoValue: function(){
+            return new Date()
+        }
+        
+    }
+});
+
+Recipes.attachSchema(RecipeSchema);
